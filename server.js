b10c6e719570034bc856f766864ceed766854024
William Kwao <williamkwao@Williams-MacBook-Pro.local>
2016-11-02 11:07:04 -0400

    Refactor: added file to handle schedule routes

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.get('/', function (req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
-
 // connect to database
 mongoose.connect(config.database);
 
@@ -46,85 +45,8 @@ require('./config/passport')(passport);
 // bundle our routes
 var apiRoutes = express.Router();
 var acc = require('./routes/accountRoutes')(apiRoutes);
+var scheduleRoutes = require('./routes/scheduleRoutes')(apiRoutes);
 
-
-
-//Schedule routes current*********************************************************************************
-apiRoutes.options('/addAvailability', cors());
-apiRoutes.post('/addAvailability', passport.authenticate('jwt', {
-    session: false
-}), function (req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        var decoded = jwt.decode(token, config.secret);
-        User.findOne({
-                _id: decoded._id
-            }, function (err, user) {
-                if (err) throw err; // To be determined
-                if (!user) {
-                    return res.status(403).send({
-                        success: false,
-                        msg: "User not found. add availability failed"
-                    });
-                } else if (req.body) {
-                    var body = req.body;
-                    var check = body.hasOwnProperty("day") && body.hasOwnProperty("start") && body.hasOwnProperty("end");
-                    if (check) {
-                        var start = body.start;
-                        var end = body.end;
-                        //Check if req body has all properties
-                        check = end.hasOwnProperty("hrs") && end.hasOwnProperty("mins") && start.hasOwnProperty("hrs") && start.hasOwnProperty("mins");
-
-                        if (check) {
-                            //Todo Validate types
-                            SchedFunctions.addAvailability(req.body, user._id);
-                            return res.status(200).send({
-                                success: true,
-                                msg: "Availability added"
-                            });
-                        }
-                    }
-
-                }
-                return res.status(403).send({
-                    success: false,
-                    msg: "Request does not contain all params"
-                });
-
-            }
-
-        )
-    }
-});
-
-apiRoutes.get('/getAvailability', passport.authenticate('jwt', {
-    session: false
-}), function (req, res) {
-    var token = getToken(req.headers);
-    if (token) {
-        var decoded = jwt.decode(token, config.secret);
-        User.findOne({
-            _id: decoded._id
-        }, function (err, user) {
-            if (err) throw err;
-
-            if (!user) {
-                return res.status(403).send({
-                    success: false,
-                    msg: 'Authentication failed. User not foun.'
-                });
-            } else {
-                   SchedFunctions.getAvailability(user._id, res);
-            }
-        });
-    } else {
-        return res.status(403).send({
-            success: false,
-            msg: 'No token provided.'
-        });
-    }
-
-});
 
 getToken = function (headers) {
     if (headers && headers.authorization) {
